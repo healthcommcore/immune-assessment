412f26776c1798036259a33f7811339b4167095b
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-10-09 12:25:38 -0400

    Rearranged bootstrap files to get dropdown menu working on mobile

--- sites/all/themes/immune_assessment/templates/html.tpl.php
+++ sites/all/themes/immune_assessment/templates/html.tpl.php
@@ -60,6 +60,18 @@
     <script src="<?php echo $base_url . '/' . drupal_get_path('theme', 'immune_assessment') . '/js/respond.min.js'; ?>"></script>
   <![endif]-->
   <?php print $scripts; ?>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-68657408-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
   <!--[if lt IE 9]>
     <body class="<?php print $classes . ' ieclass'; ?>" <?php print $attributes;?>>
